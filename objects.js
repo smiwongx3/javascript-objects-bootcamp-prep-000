f2682c63811ae48e01d580f5306e724ec4668bb5
Samantha Wong <smiwong920@gmail.com>
2019-01-26 20:09:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 var playlist = {artistName: 'song titles'} 
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+function updatePlaylist(playlist, artistName, songTitle) { 
+  playlist[artistName] = songTitle 
+    return playlist
 }
+  
 
